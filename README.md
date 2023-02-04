5a3b59383013b35b0f488e26b831ca4a10c50c2e
zb3 <onlylogout@gmail.com>
2023-02-04 18:12:17 +0100

    https://www.youtube.com/watch?v=7_8y40JEmyI

---
+++ README.md
@@ -0,0 +1,22 @@
+# Jump Outside Indent
+
+Jump before the current indent with <kbd>Alt</kbd>+<kbd>[</kbd> and after the current indent with <kbd>Alt</kbd>+<kbd>]</kbd>
+
+## Installing
+1. Manually clone this repository into the folder where extensions are installed
+2. Restart VSCode
+
+On Linux, this should do the first step:
+```
+cd ~/.vscode 2>/dev/null || cd ~/.vscode-oss
+mkdir -p extensions
+cd extensions
+git clone https://github.com/zb3/vscode-jump-outside-indent
+```
+
+## Settings
+Commands to rebind are:
+```
+jumpOutsideIndent.jumpBefore
+jumpOutsideIndent.jumpAfter
+```
---
+++ extension.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const vscode = require("vscode");
+
+function getIndentLevel(/** @type {vscode.TextEditor} */ editor, /** @type {number} */ lineNo) {
+  const line = editor.document.lineAt(lineNo);
+  return line.isEmptyOrWhitespace ? -1 : line.firstNonWhitespaceCharacterIndex;
+}
+
+function moveTo(/** @type {vscode.TextEditor} */ editor, /** @type {number} */ line, /** @type {number} */ col) {
+  const position = new vscode.Position(line, col);
+  editor.selection = new vscode.Selection(position, position);
+}
+
+function jump(/** @type {vscode.TextEditor} */ editor, /** @type {number} */ dir) {
+  let lineNo = editor.selection.start.line;
+  const currentLevel = getIndentLevel(editor, lineNo);
+
+  while (true) {
+    lineNo += dir;
+    if (lineNo < 0 || lineNo == editor.document.lineCount) {
+      break;
+    }
+
+    const newLevel = getIndentLevel(editor, lineNo);
+
+    if (newLevel != -1 && newLevel < currentLevel) {
+      moveTo(editor, lineNo, newLevel);
+      break;
+    }
+  }
+}
+
+function jumpBefore(/** @type {vscode.TextEditor} */ editor) {
+  jump(editor, -1);
+}
+
+function jumpAfter(/** @type {vscode.TextEditor} */ editor) {
+  jump(editor, 1);
+}
+
+exports.activate = function activate(/** @type {vscode.ExtensionContext} */ context) {
+  context.subscriptions.push(
+    vscode.commands.registerTextEditorCommand('jumpOutsideIndent.jumpBefore', jumpBefore),
+    vscode.commands.registerTextEditorCommand('jumpOutsideIndent.jumpAfter', jumpAfter)
+  );
+};
